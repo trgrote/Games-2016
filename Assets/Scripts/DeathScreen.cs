3a05b116518d912c102f5aa84350278fe7b15744
Taylor Grote <grotese@gmail.com>
2016-03-26 18:02:30 -0500

    Add Death

---
+++ Assets/Scripts/DeathScreen.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using InControl;
+using UnityEngine.SceneManagement;
+
+public class DeathScreen : MonoBehaviour 
+{
+	// Update is called once per frame
+	void Update () 
+	{
+		if ( Globals.State == eGameState.GameOver )
+		{
+			foreach ( InControl.InputDevice device in InControl.InputManager.Devices )
+			{
+				if ( device.GetControl( InControl.InputControlType.Start ) )
+				{
+					Globals.ReloadScene();
+					break;
+				}
+				else if ( device.GetControl( InControl.InputControlType.Back ))
+				{
+					Globals.ReturnToTitle();
+					break;
+				}
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/GameStateHandler.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameStateHandler : MonoBehaviour, IEventHandler
+{
+	void Start()
+	{
+		EventBroadcaster.registerHandler<PlayerDeathEvent>(this);
+		EventBroadcaster.registerHandler<OnGoal>(this);
+	}
+
+	public void handleEvent( IGameEvent evt )
+	{
+		if ( evt is PlayerDeathEvent )
+		{
+			// ON Death, switch state
+			Globals.State = eGameState.GameOver;
+			// display death screen
+		}
+		else if ( evt is OnGoal )
+		{
+			// Check if all players are on goals
+			bool allOnGoal = true;
+			foreach ( var obj in GameObject.FindObjectsOfType(typeof(PlayerBehavior)) )
+			{
+				PlayerBehavior player = obj as PlayerBehavior;
+				if ( player.onGoal == false )
+					allOnGoal = false;
+			}
+		}
+	}
+}
