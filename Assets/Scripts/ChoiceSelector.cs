6a0ea19429e2e7910058658afdf48882dad111dd
Taylor Grote <grotese@gmail.com>
2016-03-26 18:41:46 -0500

    Add Sound Player

--- Assets/Scripts/ChoiceSelector.cs
+++ Assets/Scripts/ChoiceSelector.cs
@@ -8,7 +8,6 @@ public class ChoiceSelector : MonoBehaviour {
 
     [SerializeField] private Image selectGame = null;
     [SerializeField] private Image selectExit = null;
-    [SerializeField] private AudioSource audio = null;
     [SerializeField] private AudioClip ding = null;
     [SerializeField] private AudioClip selectDing = null;
 
@@ -22,29 +21,22 @@ public class ChoiceSelector : MonoBehaviour {
 	void Update () {
         if (InputManager.Devices[0].DPadDown.HasChanged)
         {
-            if (!audio.isPlaying)
-            {
-                audio.clip = ding;
-                audio.Play();
-            }
+            var audio = GameObject.FindObjectOfType<SoundPlayer>();
+            audio.PlaySound(ding);
             selectGame.enabled = false;
             selectExit.enabled = true;
         }
         if (InputManager.Devices[0].DPadUp.HasChanged)
         {
-            if (!audio.isPlaying)
-            {
-                audio.clip = ding;
-                audio.Play();
-            }
+            var audio = GameObject.FindObjectOfType<SoundPlayer>();
+            audio.PlaySound(ding);
             selectGame.enabled = true;
             selectExit.enabled = false;
         }
         if (InputManager.Devices[0].Action1.IsPressed)
         {
-            audio.Stop();
-            audio.clip = selectDing;
-            audio.Play();
+            var audio = GameObject.FindObjectOfType<SoundPlayer>();
+            audio.PlaySound(selectDing);
             if (selectGame.enabled == true)
             {
                 Globals.LoadNextScene();
