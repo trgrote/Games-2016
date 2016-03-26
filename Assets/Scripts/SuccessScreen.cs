37acd564acf812ca20557606f19cfe6917aa0bd4
Taylor Grote <grotese@gmail.com>
2016-03-26 18:08:44 -0500

    Add Victory Screen

---
+++ Assets/Scripts/SuccessScreen.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using InControl;
+
+public class SuccessScreen : MonoBehaviour 
+{
+	// Update is called once per frame
+	void Update () 
+	{
+		foreach ( InControl.InputDevice device in InControl.InputManager.Devices )
+			{
+				if ( device.GetControl( InControl.InputControlType.Start ))
+				{
+					Globals.ReturnToTitle();
+					break;
+				}
+			}
+	}
+}
