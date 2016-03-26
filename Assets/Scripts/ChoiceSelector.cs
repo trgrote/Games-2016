c96887370c5b6cbd62e087ca0c9d9a10ed891631
Ross <Ross>
2016-03-26 17:45:39 -0500

    Add Title Screen

---
+++ Assets/Scripts/ChoiceSelector.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using InControl;
+using System.Collections;
+
+public class ChoiceSelector : MonoBehaviour {
+
+    [SerializeField] private Image selectGame = null;
+    [SerializeField] private Image selectExit = null;
+    [SerializeField] private AudioSource audio = null;
+
+	// Use this for initialization
+	void Start () {
+        selectGame.enabled = true;
+        selectExit.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (InputManager.Devices[0].DPadDown.HasChanged)
+        {
+            selectGame.enabled = false;
+            selectExit.enabled = true;
+        }
+        if (InputManager.Devices[0].DPadUp.HasChanged)
+        {
+            selectGame.enabled = true;
+            selectExit.enabled = false;
+        }
+	}
+}
