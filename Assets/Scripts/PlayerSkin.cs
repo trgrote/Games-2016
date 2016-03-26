9cb083de2fa3a70f380df7a8ead1e4c08df4fe46
Taylor Grote <grotese@gmail.com>
2016-03-26 17:10:40 -0500

    Add Player SKins

---
+++ Assets/Scripts/PlayerSkin.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(PlayerBehavior), typeof(SpriteRenderer))]
+public class PlayerSkin : MonoBehaviour 
+{
+	[SerializeField] private Sprite PlayerOne;
+	[SerializeField] private Sprite PlayerTwo;
+
+	// Use this for initialization
+	void Start () 
+	{
+		var behavior = GetComponent<PlayerBehavior>();
+		var sprite = GetComponent<SpriteRenderer>();
+
+		switch ( behavior.player )
+		{
+			case PlayerSelect.PLAYER1:
+				sprite.sprite = PlayerOne;
+				break;
+			case PlayerSelect.PLAYER2:
+				sprite.sprite = PlayerTwo;
+				break;
+		}
+	}
+}
