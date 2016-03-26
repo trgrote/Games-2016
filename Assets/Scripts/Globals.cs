dcc717d742072974430d1b5a567c88e67ca16ee3
Taylor Grote <grotese@gmail.com>
2016-03-26 11:01:55 -0500

    Add Gameover state

--- Assets/Scripts/Globals.cs
+++ Assets/Scripts/Globals.cs
@@ -4,7 +4,8 @@ using System.Collections;
 public enum eGameState
 {
 	GameMode,     // Default Game Mode, players moving around
-	Flipping      // Paused Gamed Mode, players can't move
+	Flipping,     // Paused Gamed Mode, players can't move
+	GameOver      // Game Paused for Players because they died, press X to restart
 }
 
 public static class Globals
