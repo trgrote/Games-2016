591eb8803a1b95b55ba744aa0411508b6430d22f
Taylor Grote <grotese@gmail.com>
2016-03-26 10:53:07 -0500

    Add Global static class w/ Game state

---
+++ Assets/Scripts/Globals.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum eGameState
+{
+	GameMode,     // Default Game Mode, players moving around
+	Flipping      // Paused Gamed Mode, players can't move
+}
+
+public static class Globals
+{
+	public static eGameState State = eGameState.GameMode;
+}
