3b93fa7a5e6eea48636e98382d718a1f5fddef65
Taylor Grote <grotese@gmail.com>
2016-03-27 13:42:19 -0500

    Fix tabs on AutoSwapScreen

--- Assets/Scripts/AutoSwapScreen.cs
+++ Assets/Scripts/AutoSwapScreen.cs
@@ -2,36 +2,36 @@
 using UnityEngine.UI;
 using System.Collections;
 
-public class AutoSwapScreen : MonoBehaviour 
+public class AutoSwapScreen : MonoBehaviour
 {
-    [SerializeField] private int delayInSeconds = 1;
+	[SerializeField] private int delayInSeconds = 1;
 
-    private float currentCounter = 0f;
-    private QuadFlipper flipper = null;
-    private Image bar = null;
+	private float currentCounter = 0f;
+	private QuadFlipper flipper = null;
+	private Image bar = null;
 
 	// Use this for initialization
-	void Start () 
-    {
-        flipper = GameObject.FindObjectOfType<QuadFlipper>();
-        bar = this.GetComponent<Image>();
-        currentCounter = delayInSeconds;
+	void Start ()
+	{
+		flipper = GameObject.FindObjectOfType<QuadFlipper>();
+		bar = this.GetComponent<Image>();
+		currentCounter = delayInSeconds;
 	}
-	
-	// Update is called once per frame
-	void Update () 
-    {
-        // If we aren't in game mode, then don't countdown
-        if ( Globals.State != eGameState.GameMode )
-            return;
 
-        currentCounter -= Time.deltaTime;
-        bar.fillAmount = currentCounter / delayInSeconds;
-        if (currentCounter <= 0)
-        {
-            EventBroadcaster.broadcastEvent( new FlipBeginEvent() );
-            flipper.PerformQuadSwitch();
-            currentCounter = delayInSeconds;
-        }
+	// Update is called once per frame
+	void Update ()
+	{
+		// If we aren't in game mode, then don't countdown
+		if ( Globals.State != eGameState.GameMode )
+			return;
+
+		currentCounter -= Time.deltaTime;
+		bar.fillAmount = currentCounter / delayInSeconds;
+		if (currentCounter <= 0)
+		{
+			EventBroadcaster.broadcastEvent( new FlipBeginEvent() );
+			flipper.PerformQuadSwitch();
+			currentCounter = delayInSeconds;
+		}
 	}
 }
