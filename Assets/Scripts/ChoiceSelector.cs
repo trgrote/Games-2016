6d312444aafbac6dae343034eccfec73a9b1b96f
Taylor Grote <grotese@gmail.com>
2016-03-30 22:30:52 -0500

    Update choice select to allow start
    Update choice select to allow start, allow any controllers to move icon, and
    allow any controllers to press either start or 'A'

--- Assets/Scripts/ChoiceSelector.cs
+++ Assets/Scripts/ChoiceSelector.cs
@@ -4,47 +4,52 @@ using UnityEngine.SceneManagement;
 using InControl;
 using System.Collections;
 
-public class ChoiceSelector : MonoBehaviour {
-
-    [SerializeField] private Image selectGame = null;
-    [SerializeField] private Image selectExit = null;
-    [SerializeField] private AudioClip ding = null;
-    [SerializeField] private AudioClip selectDing = null;
+public class ChoiceSelector : MonoBehaviour
+{
+	[SerializeField] private Image selectGame = null;
+	[SerializeField] private Image selectExit = null;
+	[SerializeField] private AudioClip ding = null;
+	[SerializeField] private AudioClip selectDing = null;
 
 	// Use this for initialization
-	void Start () {
-        selectGame.enabled = true;
-        selectExit.enabled = false;
+	void Start ()
+	{
+		selectGame.enabled = true;
+		selectExit.enabled = false;
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
-        if (InputManager.Devices[0].DPadDown.HasChanged)
-        {
-            var audio = GameObject.FindObjectOfType<SoundPlayer>();
-            audio.PlaySound(ding);
-            selectGame.enabled = false;
-            selectExit.enabled = true;
-        }
-        if (InputManager.Devices[0].DPadUp.HasChanged)
-        {
-            var audio = GameObject.FindObjectOfType<SoundPlayer>();
-            audio.PlaySound(ding);
-            selectGame.enabled = true;
-            selectExit.enabled = false;
-        }
-        if (InputManager.Devices[0].Action1.IsPressed)
-        {
-            var audio = GameObject.FindObjectOfType<SoundPlayer>();
-            audio.PlaySound(selectDing);
-            if (selectGame.enabled == true)
-            {
-                Globals.LoadNextScene();
-            }
-            if (selectExit.enabled == true)
-            {
-                Application.Quit();
-            }
-        }
+	void Update ()
+	{
+		foreach ( InControl.InputDevice device in InControl.InputManager.Devices )
+		{
+			if (device.DPadDown.HasChanged)
+			{
+				var audio = GameObject.FindObjectOfType<SoundPlayer>();
+				audio.PlaySound(ding);
+				selectGame.enabled = false;
+				selectExit.enabled = true;
+			}
+			if (device.DPadUp.HasChanged)
+			{
+				var audio = GameObject.FindObjectOfType<SoundPlayer>();
+				audio.PlaySound(ding);
+				selectGame.enabled = true;
+				selectExit.enabled = false;
+			}
+			if (device.Action1.IsPressed || device.GetControl( InControl.InputControlType.Start ))
+			{
+				var audio = GameObject.FindObjectOfType<SoundPlayer>();
+				audio.PlaySound(selectDing);
+				if (selectGame.enabled == true)
+				{
+					Globals.LoadNextScene();
+				}
+				if (selectExit.enabled == true)
+				{
+					Application.Quit();
+				}
+			}
+		}
 	}
 }
