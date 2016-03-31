98088465a49123a580e170e56f1e96f537885329
Taylor Grote <grotese@gmail.com>
2016-03-30 22:15:20 -0500

    Add Go to next scene script
    Add go to next scene script to aid in making the story levels jump to the
    following scene without having to know where they are

---
+++ Assets/Scripts/NextSceneOnStart.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NextSceneOnStart : MonoBehaviour
+{
+	// Update is called once per frame
+	void Update ()
+	{
+		foreach ( InControl.InputDevice device in InControl.InputManager.Devices )
+		{
+			if ( device.GetControl( InControl.InputControlType.Start ))
+			{
+				Globals.LoadNextScene();
+				break;
+			}
+		}
+	}
+}
